fef5a6abdffd1f30af90b2b7b2b9c8f8ba30b111
Sharanprasad <sjenweri@gmail.com>
2026-07-23 20:52:52 +0530

    Initial commit:6th change

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const button = document.getElementById("btn");
 const countDisplay = document.getElementById("count");
+const resetButton = document.getElementById("Reset");
+
 let count = 0;
 
-button.addEventListener("click", function (){
-    count ++;
+button.addEventListener("click", function () {
+    count++;
+    countDisplay.textContent = count;
+});
+
+resetButton.addEventListener("click", function () {
+    alert("Reset button Clicked");
+    count = 0;
     countDisplay.textContent = count;
 });
